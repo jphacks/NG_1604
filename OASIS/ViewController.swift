1417e56e803bd32573c9937805558565d2c9e538
Yasuaki Goto <yasu1003@gmail.com>
2016-10-31 02:45:28 +0900

    [add] project

---
+++ OASIS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OASIS
+//
+//  Created by 後藤誉昌 on 2016/10/31.
+//  Copyright © 2016年 othlotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
