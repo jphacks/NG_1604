dbb570d69086533d6d457c8fad954455425c8cae
Yasuaki Goto <yasu1003@gmail.com>
2016-11-03 16:55:36 +0900

    [add] entity

---
+++ OASIS/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  OASIS
+//
+//  Created by 後藤誉昌 on 2016/11/03.
+//  Copyright © 2016年 othlotech. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+
+    let user: User
+    let comment: String
+    let date: Date
+}
---
+++ OASIS/Room.swift
@@ -0,0 +1,15 @@
+//
+//  Room.swift
+//  OASIS
+//
+//  Created by 後藤誉昌 on 2016/11/03.
+//  Copyright © 2016年 othlotech. All rights reserved.
+//
+
+import Foundation
+
+struct Entity {
+
+    let user: User
+    let messages: [Message]
+}
