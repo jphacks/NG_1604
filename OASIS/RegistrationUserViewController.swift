1e37d2cb5abb86b7a17d967e701dcfc4b7936292
Yazaki Yuto <yuto.yazaki@gmail.com>
2016-11-03 19:13:21 +0900

    [update] FB Graph API叩いた

--- OASIS/RegistrationUserViewController.swift
+++ OASIS/RegistrationUserViewController.swift
@@ -8,26 +8,63 @@
 
 import UIKit
 import Firebase
+import FacebookCore
 
 class RegistrationUserViewController: UIViewController, Storyboardable, ErrorHandlable {
 
     // MARK: - Property
     static let storyboardName = "RegistrationUser"
+//    struct UserProfileRequest: GraphRequestProtocol {
+//        let graphPath = "me"
+//        let parameters: [String : Any]? = [:]
+//        let accessToken: AccessToken?
+//        let httpMethod: GraphRequestHTTPMethod = .GET
+//        let apiVersion: GraphAPIVersion = .defaultVersion
+//
+//        init(token: String) {
+//            self.accessToken = AccessToken(authenticationToken: token)
+//        }
+//        
+//    }
 
     // MARK: - Action
     @IBAction private func submitBtnDidTap(_ sender: UIButton) {
         guard let user = FIRAuth.auth()?.currentUser else { return }
 
-        let request = user.profileChangeRequest()
-        request.displayName = "ヒデちゃん"
-        request.photoURL = URL(string: "http://www.othlo.tech/images/members/hide.png")
-        request.commitChanges { error in
-            if let error = error {
-                self.handle(error: error)
-            } else {
-                let next = RegistrationTimeTableViewController.makeFromStoryboard()
-                self.navigationController?.pushViewController(next, animated: true)
-            }
+        user.getTokenWithCompletion { (token, error) in
+            guard let token = token else { return }
+            print(token)
+            self.fetchUserProfile(token: AccessToken(authenticationToken: token))
         }
+//        let request = user.profileChangeRequest()
+//        request.displayName = "ヒデちゃん"
+//        request.photoURL = URL(string: "http://www.othlo.tech/images/members/hide.png")
+//        request.commitChanges { error in
+//            if let error = error {
+//                self.handle(error: error)
+//            } else {
+//                let next = RegistrationTimeTableViewController.makeFromStoryboard()
+//                self.navigationController?.pushViewController(next, animated: true)
+//            }
+//        }
+    }
+
+    // MARK: - Private
+    private func fetchUserProfile(token: AccessToken) {
+        let graphRequest = GraphRequest(graphPath: "me", parameters: [:], accessToken: token, httpMethod: .GET, apiVersion: .defaultVersion)
+        graphRequest.start { (urlResponse, requestResult) in
+            switch requestResult {
+            case .failed(let error):
+                print("error in graph request:", error)
+                break
+            case .success(let graphResponse):
+                if let responseDictionary = graphResponse.dictionaryValue {
+                    dump(responseDictionary)
+                }
+            }
+            
+        }
+//        let connection = GraphRequestConnection()
+//        connection.add(<#T##request: T##T#>, batchEntryName: <#T##String?#>, completion: <#T##(HTTPURLResponse?, GraphRequestResult<T>) -> Void?##(HTTPURLResponse?, GraphRequestResult<T>) -> Void?##(HTTPURLResponse?, GraphRequestResult<T>) -> Void#>)
     }
 }
