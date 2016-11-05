ef5fa90c6370f7e3e03af7026a76eb0b87e08ab3
Yasuaki Goto <yasu1003@gmail.com>
2016-11-05 18:10:34 +0900

    [add] api client

--- OASIS/WebAPI.Users.swift
+++ OASIS/WebAPI.Users.swift
@@ -18,10 +18,15 @@ extension WebAPI {
 
         static func create(image: String, name: String, gender: String, univ: String, department: String, grade: Int, profile: String) -> UserTask {
             return UserTask { _, fulfill, reject, _ in
+                guard let uuid = WebAPI.uuid else {
+                    reject(AppError.unauthorized)
+                    return
+                }
+
                 let url = AppConfig.WebAPI.BaseURL + "/users"
 
                 let params: Parameters = [
-                    "uuid": "test",
+                    "uuid": uuid,
                     "department": department,
                     "gender": gender,
                     "name": name,
@@ -29,6 +34,126 @@ extension WebAPI {
                     "profile": profile,
                     "profile_img": image,
                     "univ_name": univ
+                ]
+
+                Alamofire.request(url, method: .post, parameters: params)
+                    .responseJSON { response in
+                        if let error = response.result.error {
+                            reject(error)
+                        }
+
+                        if let json = response.result.value as? [String: String], let user = try? User.decodeValue(json) {
+                            fulfill(user)
+                        }
+
+                        reject(AppError.unknown)
+                }
+            }
+        }
+
+        static func update(uuid: String) -> UserTask {
+            return UserTask { _, fulfill, reject, _ in
+                let url = AppConfig.WebAPI.BaseURL + "/users/" + uuid
+
+                Alamofire.request(url, method: .post)
+                    .responseJSON { response in
+                        if let error = response.result.error {
+                            reject(error)
+                        }
+
+                        if let json = response.result.value as? [String: String], let user = try? User.decodeValue(json) {
+                            fulfill(user)
+                        }
+
+                        reject(AppError.unknown)
+                }
+            }
+        }
+
+        static func show(uuid: String) -> UserTask {
+            return UserTask { _, fulfill, reject, _ in
+                let url = AppConfig.WebAPI.BaseURL + "/users/" + uuid
+
+                Alamofire.request(url)
+                    .responseJSON { response in
+                        if let error = response.result.error {
+                            reject(error)
+                        }
+
+                        if let json = response.result.value as? [String: String], let user = try? User.decodeValue(json) {
+                            fulfill(user)
+                        }
+
+                        reject(AppError.unknown)
+                }
+            }
+        }
+
+        static func remove() -> UserTask {
+            return UserTask { _, fulfill, reject, _ in
+                guard let uuid = WebAPI.uuid else {
+                    reject(AppError.unauthorized)
+                    return
+                }
+
+                let url = AppConfig.WebAPI.BaseURL + "/users/" + uuid + "/remove"
+
+                Alamofire.request(url, method: .post)
+                    .responseJSON { response in
+                        if let error = response.result.error {
+                            reject(error)
+                        }
+
+                        if let json = response.result.value as? [String: String], let user = try? User.decodeValue(json) {
+                            fulfill(user)
+                        }
+
+                        reject(AppError.unknown)
+                }
+            }
+        }
+
+        static func like(opponent: String) -> UserTask {
+            return UserTask { _, fulfill, reject, _ in
+                guard let uuid = WebAPI.uuid else {
+                    reject(AppError.unauthorized)
+                    return
+                }
+
+                let url = AppConfig.WebAPI.BaseURL + "/users/" + uuid + "/like"
+
+                let params: Parameters = [
+                    "opponent_uuid": opponent,
+                    "match_result": true
+                ]
+
+                Alamofire.request(url, method: .post, parameters: params)
+                    .responseJSON { response in
+                        if let error = response.result.error {
+                            reject(error)
+                        }
+
+                        if let json = response.result.value as? [String: String], let user = try? User.decodeValue(json) {
+                            fulfill(user)
+                        }
+
+                        reject(AppError.unknown)
+                }
+            }
+        }
+
+        static func dislike(opponent: String) -> UserTask {
+            return UserTask { _, fulfill, reject, _ in
+                guard let uuid = WebAPI.uuid else {
+                    reject(AppError.unauthorized)
+                    return
+                }
+
+                let url = AppConfig.WebAPI.BaseURL + "/users/" + uuid + "/like"
+
+                let params: Parameters = [
+                    "opponent_uuid": opponent,
+                    "match_result": false
                 ]
 
                 Alamofire.request(url, method: .post, parameters: params)
