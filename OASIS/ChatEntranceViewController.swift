5af517404bf8e657ad0682c07b68332d8da497aa
Yasuaki Goto <yasu1003@gmail.com>
2016-11-06 11:51:33 +0900

    [fix] chat

--- OASIS/ChatEntranceViewController.swift
+++ OASIS/ChatEntranceViewController.swift
@@ -71,6 +71,7 @@ extension ChatEntranceViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let next = ChatRoomViewController.makeFromStoryboard()
         next.hidesBottomBarWhenPushed = true
+        next.room = rooms[indexPath.row]
         navigationController?.pushViewController(next, animated: true)
     }
 }
--- OASIS/ChatRoomViewController.swift
+++ OASIS/ChatRoomViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 import JSQMessagesViewController
 
 class ChatRoomViewController: JSQMessagesViewController, Storyboardable {
@@ -14,26 +15,20 @@ class ChatRoomViewController: JSQMessagesViewController, Storyboardable {
     // MARK: - Property
     static let storyboardName = "ChatRoom"
 
-    var messages = Mock.messages
+    private var incomingBubble: JSQMessagesBubbleImage!
+    private var outgoingBubble: JSQMessagesBubbleImage!
+    private var incomingAvatar: JSQMessagesAvatarImage!
+    private var outgoingAvatar: JSQMessagesAvatarImage!
 
-    var incomingBubble: JSQMessagesBubbleImage!
-    var outgoingBubble: JSQMessagesBubbleImage!
-    var incomingAvatar: JSQMessagesAvatarImage!
-    var outgoingAvatar: JSQMessagesAvatarImage!
+    var room: Room!
+    private var messages: [JSQMessage] = []
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        inputToolbar!.contentView!.leftBarButtonItem = nil
-        automaticallyScrollsToMostRecentMessage = true
-
-        senderId = "user1"
-        senderDisplayName = "ひでちゃん"
-
-        let bubbleFactory = JSQMessagesBubbleImageFactory()
-        incomingBubble = bubbleFactory?.incomingMessagesBubbleImage(with: .jsq_messageBubbleLightGray())
-        outgoingBubble = bubbleFactory?.outgoingMessagesBubbleImage(with: .jsq_messageBubbleGreen())
+        setupChat()
+        setupFirebase()
+        title = room.title
     }
 
     // MARK: - Public
@@ -41,6 +36,7 @@ class ChatRoomViewController: JSQMessagesViewController, Storyboardable {
         let message = JSQMessage(senderId: senderId, senderDisplayName: senderDisplayName, date: date, text: text)!
         messages.append(message)
         finishSendingMessage(animated: true)
+        sendTextToDb(text: text)
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -59,5 +55,54 @@ class ChatRoomViewController: JSQMessagesViewController, Storyboardable {
     override func collectionView(_ collectionView: JSQMessagesCollectionView, avatarImageDataForItemAt indexPath: IndexPath) -> JSQMessageAvatarImageDataSource? {
         let message = messages[indexPath.item]
         return message.senderId == senderId ? outgoingAvatar : incomingAvatar
+    }
+
+    // MARK: - Private
+    private func setupChat() {
+        guard let name = FIRAuth.auth()?.currentUser?.displayName else {
+            return
+        }
+
+        inputToolbar!.contentView!.leftBarButtonItem = nil
+        automaticallyScrollsToMostRecentMessage = true
+
+        senderId = name
+        senderDisplayName = name
+
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        incomingBubble = bubbleFactory?.incomingMessagesBubbleImage(with: .jsq_messageBubbleLightGray())
+        outgoingBubble = bubbleFactory?.outgoingMessagesBubbleImage(with: .jsq_messageBubbleGreen())
+    }
+
+    private func setupFirebase() {
+        let rootRef = FIRDatabase.database().reference().child("messages").child(room.id)
+
+        rootRef.queryLimited(toLast: 100).observe(FIRDataEventType.childAdded, with: { snapshot in
+            guard let dic = snapshot.value as? [String : AnyObject],
+                let name = dic["name"] as? String,
+                let text = dic["text"] as? String else {
+                    return
+            }
+
+            let message = JSQMessage(senderId: name, displayName: name, text: text)!
+            self.messages.append(message)
+            self.finishReceivingMessage()
+        })
+    }
+
+    private func sendTextToDb(text: String) {
+        guard let name = FIRAuth.auth()?.currentUser?.displayName else {
+            return
+        }
+
+        let rootRef = FIRDatabase.database().reference().child("messages").child(room.id)
+
+        let post = [
+            "name": name,
+            "text": text
+        ]
+
+        let postRef = rootRef.childByAutoId()
+        postRef.setValue(post)
     }
 }
